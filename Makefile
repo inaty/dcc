a8a9843f4dfdacef79cee84f0146b6483a777062
inaty <inaty0610@gmail.com>
2020-04-16 00:35:36 +0900

    add minimal build tool

---
+++ Makefile
@@ -0,0 +1,11 @@
+CFLAG=-std=c11 -g -static
+
+dcc: dcc.c
+
+test: dcc
+		./test.sh
+
+clean:
+		rm -f dcc *.o *~ tmp*
+
+.PHONY: test clean
--- test.sh
+++ test.sh
@@ -3,7 +3,6 @@ assert(){
     expected="$1"
     input="$2"
 
-    cc -o dcc dcc.c
     ./dcc "$input" > tmp.s 
     cc -o tmp tmp.s
     ./tmp
